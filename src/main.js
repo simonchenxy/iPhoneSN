1ec4342f62ef6dee5a88f9c29c796e5f741cabe4
1162643@qq.com <1162643@qq.com>
2017-10-18 16:49:40 +0800

    fix issue

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
 Vue.use(VueResource)
+
+
 
 /* eslint-disable no-new */
 new Vue({
